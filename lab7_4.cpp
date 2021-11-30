660f6c5ffa7d4353b96ddb1b32a825dba331fb33
tanawat640610678 <thanawat_chai@cmu.ac.th>
2021-11-30 18:44:02 +0700

    lab7_4.cpp Complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,19 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    double grade;
+      cout << "Please input GPA: ";
+      cin >> grade;
+      
+      int x=0;
+      while(x<10){
+          
+          if(grade <= gpa[i]){
+          cout << id[i] << " " << name[i ] << " " << gpa[i]<< "\n";
+      } i++;
+		
+	return 0;
+}
 		
 	return 0;
 }
